e84f1842e0dc280ec5c68eee08d53ecda3a0b0be
syarawldn <waldanasyara@gmail.com>
2021-10-26 11:52:32 +0700

    Toko Sepatu

---
+++ application/views/view-data-toko.php
@@ -0,0 +1,60 @@
+
+  <div class="container">
+ <!-- Outer Row -->
+ <div class="row justify-content-center">
+ <div class="col-lg-7">
+ <div class="card o-hidden border-0 shadow-lg my-5">
+ <div class="card-body p-0">
+ <!-- Nested Row within Card Body -->
+ <div class="row">
+ <div class="col-lg">
+ <div class="p-5">
+ <div class="text-center">
+ <h1 class="h4 text-gray-900 mb4"><font color="7F00FF"><b><u>Hasil Data Toko Sepatu,</font></b></u></h1></h1>
+ </div>
+   <div class="mb-3">
+     <label>Nama</label>
+     <input class="form-control" value="<?= $nama; ?>" disabled>
+   </div>
+
+   <div class="mb-3">
+     <label>Nomor Handphone</label>
+     <input class="form-control" value="<?= $nohp; ?>" disabled>
+   </div>
+
+ <div class="mb-3">
+        <label>Merk Sepatu</label>
+        <input class="form-control" value="<?= $merk; ?>" disabled>
+
+    </div>
+    <div class="mb-3">
+           <label>Ukuran Sepatu</label>
+           <input class="form-control" value="<?= $size; ?>" disabled>
+
+       </div>
+       <div class="mb-3">
+
+											<label>Total Harga</label>
+                                        <div class="input-group">
+                                        <div class="input-group-prepend"><span class="input-group-text text-primary">Rp</span></div>
+			                                    <input type="text" class="form-control" value="<?= format_rp($harga); ?>" disabled> </div>
+		                                    </div>
+        <a href="<?= base_url('Sepatu');?>" type="submit" class="btn btn-warning btn-user btn-block">Kembali
+        </a>
+       <hr>
+       <div class="container my-auto">
+           <div class="copyright text-center my-auto">
+              <!--- <span>Copyright &copy; Your Website 2020</span> !-->
+                Crafted by <a href="https://instagram.com/syarawldn" target="_blank"> Syara Waldana.</a>
+
+           </div>
+       </div>
+      </div>
+
+     </div>
+    </div>
+   </div>
+ </div>
+ </div>
+ </div>
+</div>
